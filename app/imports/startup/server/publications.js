73a2e71e42fed86eb6cf9ae6fd54e781194d25fc
sikippy <sikippy_kipkip@yahoo.com>
2018-07-25 14:34:59 +0700

    Update publications.js

--- app/imports/startup/server/publications.js
+++ app/imports/startup/server/publications.js
@@ -1,6 +1,12 @@
 import { EventData } from '../../api/eventdata/eventdata.js';
+//import { eventlogin } from '../../api/eventlogin/eventlogin.js' 
+//import { eventlogin } from '/imports/api/eventlogin/eventlogin.js';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('EventData', function publishStudentData() {
   return EventData.find();
+});
+
+Meteor.publish('eventlogin', function eventlogin() {
+  return eventlogin.find();
 });
