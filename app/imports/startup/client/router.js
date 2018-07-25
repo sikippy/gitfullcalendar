c680e4d2d9bd24fd4be9f2392a5be06cda3c1d07
sikippy <sikippy_kipkip@yahoo.com>
2018-07-25 14:34:20 +0700

    Update router.js

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,8 +2,15 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
-  name: 'Calendar_Page',
+  name: 'App_Login',
   action() {
+    BlazeLayout.render('App_Layout', { main: 'App_Login' });
+  },
+});
+
+FlowRouter.route('/Calendar/:name', {
+  name: 'Calendar_Page',
+  action(params) {
     BlazeLayout.render('App_Layout', { main: 'Calendar_Page' });
   },
 });
